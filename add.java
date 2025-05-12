5197450be28d640079bbe2b4f8c728d29f8b58a2
Ragul9025 <ragul6961@gmail.com>
2025-05-12 10:57:25 +0530

    Create add.java
    Signed-off-by: Ragul <ragul6961@gmail.com>

--- add.java
+++ add.java
@@ -1,9 +1,20 @@
-public class Main {
+public class Main
+{
+
     public static void main(String[] args)
     {
-         int a=10;
+    /*     int a=10;
          int b=15;
          System.out.println(a+b);
+         System.out.println("true ")*/
+
+        for (int i=0;i<10;i++)
+        {
+            if (i%2==0) {
+                System.out.println("its even");
+
+            }
+        }
         }
 
 }
